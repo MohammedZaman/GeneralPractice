233766b9bf318cb64b38f9c738a12c584f7ac864
mohammedzaman <mohamed_s_zaman@hotmail.co.uk>
2019-08-06 18:21:26 +0100

    I have set up a subclass of programmer which inherits from Employee.

--- src/Inheritance/Main.java
+++ src/Inheritance/Main.java
@@ -12,10 +12,35 @@ package Inheritance;
 public class Main {
     public static void main(String[] args) {
         
+      Programmer programmer = new Programmer();
+      System.out.println("The programmer salary is £"+programmer.getSalary());
+      System.out.println("The bonus for this year is £"+programmer.getBonus());
+      
+      
+        
     }
 }
 
 class Employee{
-    private double salary;
+    private double salary = 150000.0;
     
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    } 
+}
+
+class Programmer extends Employee{
+    private double bonus = 2000;
+    
+    public double getBonus() {
+        return bonus;
+    }
+
+    public void setBonus(double bonus) {
+        this.bonus = bonus;
+    }
 }
