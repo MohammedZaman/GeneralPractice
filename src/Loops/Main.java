c6c71eb03106cc092fcc7f150bb1029526960cc2
mohammedzaman <mohamed_s_zaman@hotmail.co.uk>
2019-08-08 16:22:54 +0100

    I have moved onto nested loops. This mainly used for 2D Arrays

--- src/Loops/Main.java
+++ src/Loops/Main.java
@@ -11,8 +11,27 @@ package Loops;
  */
 public class Main {
     public static void main(String[] args) {
-        forLoops();
+        //forLoops();
+        nestedLoops();
          
+    }
+    
+    
+    private static void nestedLoops(){
+    int[][] graph = new int[5][5]; 
+    
+    for(int i = 0 ; i < graph.length; i++){
+        for(int j = 0; j < graph.length;j++){
+            graph[i][j] = 1;
+        }
+    }
+     for(int i = 0 ; i < graph.length; i++){
+        System.out.println(System.lineSeparator());
+        for(int j = 0; j < graph.length;j++){
+            System.out.print(graph[i][j]);
+        }
+    }
+    
     }
     
     
