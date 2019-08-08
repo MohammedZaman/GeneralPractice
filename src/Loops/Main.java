1c4b33d40932cec62a69ceff66fa52806d78f7b5
mohammedzaman <mohamed_s_zaman@hotmail.co.uk>
2019-08-08 16:13:50 +0100

    There are loops with with breaks and continue statements.

--- src/Loops/Main.java
+++ src/Loops/Main.java
@@ -11,15 +11,38 @@ package Loops;
  */
 public class Main {
     public static void main(String[] args) {
-        
+        forLoops();
+         
+    }
+    
+    
+    private static void forLoops(){   
+      System.out.println("Standard for Loop");  
       for(int i = 0; i< 10; i++){
-          System.out.println("Hello");
+             System.out.println("hello " + (i+1));
       }
       
+      System.out.println("for Loop with break ");  
+      for(int i = 0; i < 10; ++i){
+             if(i == 4){
+                 break;
+             }
+             System.out.println("hello " + (i+1));
+             
+      }
       
-      
-      
-        
+      System.out.println("for loop with continue  ");       
+      for(int i = 0; i < 10; ++i){
+             if(i == 3){
+                 continue;
+             }else{
+                 System.out.println("hello " + (i+1));
+             }
+      } 
     }
+    
+    
+    
 }
+
 
