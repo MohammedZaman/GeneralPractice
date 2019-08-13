774e21c304fd60e367dfbbc21185f5ef6b90156d
mohammedzaman <mohamed_s_zaman@hotmail.co.uk>
2019-08-13 20:35:04 +0100

    Today i will be covering some Concurrency. So far i have covered Runnable and Thread class. Also i have covered .Join function

---
+++ src/Threads/Main.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Threads;
+
+
+/**
+ *
+ * @author mohammedzaman
+ */
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        
+        
+        Thread thread = new RandomThread("ThreadY");
+        thread.start();
+        
+        for(int i = 0; i< 7; i++){
+        System.out.println("I am Main Thread " + i );
+        if(i == 3){
+            thread.join();
+            System.out.println("Threads Joined");
+        }
+        }
+        
+        
+        
+        
+       
+      
+        
+  
+    }
+    
+}
---
+++ src/Threads/RandomThread.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Threads;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author mohammedzaman
+ */
+public class RandomThread extends Thread {
+
+    private String name;
+    public RandomThread(String nameOfThread){
+    this.name = nameOfThread;
+    
+    }
+    @Override
+    public void run() {
+       for(int i = 0; i< 7; i++){
+       System.out.println("I am "+ name + i );
+       }
+    }
+    
+    
+}
