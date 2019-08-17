4f711cd9fe422668bb71afaf0ae74d24d4b1a0b1
mohammedzaman <mohamed_s_zaman@hotmail.co.uk>
2019-08-17 04:03:52 +0100

    Today I covered Enums

---
+++ src/Enums/Main.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Enums;
+
+import encapsulation.*;
+
+/**
+ *
+ * @author mohammedzaman
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       System.out.println(Shapes.Circle);
+       System.out.println(Shapes.Square);
+       System.out.println(Shapes.Triangle);
+    }
+    
+}
