a358517604047002629196462e7eaf4e10f22d89
mohammedzaman <mohamed_s_zaman@hotmail.co.uk>
2019-08-27 15:09:56 +0100

    I have set up a parent class in order to implement factory pattern

---
+++ src/FactoryPattern/Shape.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package FactoryPattern;
+
+/**
+ *
+ * @author mohammedzaman
+ */
+public class Shape {
+    private String shapeType;
+    private double shapeSize;
+
+    public String getShapeType() {
+        return shapeType;
+    }
+
+    public void setShapeType(String shapeType) {
+        this.shapeType = shapeType;
+    }
+
+    public double getShapeSize() {
+        return shapeSize;
+    }
+
+    public void setShapeSize(double shapeSize) {
+        this.shapeSize = shapeSize;
+    }
+    
+    
+}
